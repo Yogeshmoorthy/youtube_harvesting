205f57bf3ea3289729673e082338fd4696f3e7f7
Yogesh <56802971+Yogeshmoorthy@users.noreply.github.com>
2023-06-23 01:06:54 +0530

    Add files via upload

---
+++ pages/👨‍💻About.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title('Myself')
+st.subheader('📮: yogeshmoorthy.p@gmail.com')
+st.subheader('🖇️: https://www.linkedin.com/in/yogesh-m-73094a1a1')
+st.subheader('💻: https://www.github.com/Yogeshmoorthy')
